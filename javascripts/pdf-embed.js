61537a1ca9c939ec246ad2dae4c6d6590c408b4f
Kyle Hubbard <khubb74@gmail.com>
2016-08-22 16:18:09 -0700

    display inline resume with iframe

---
+++ javascripts/pdf-embed.js
@@ -0,0 +1,8 @@
+
+var pdf = new PDFObject({
+  url: "https://www.pdf-archive.com/2016/08/23/resume-kyle-hubbard-aug-2/",
+  id: "pdfRendered",
+  pdfOpenParams: {
+    view: "FitH"
+  }
+}).embed("pdfRenderer");
